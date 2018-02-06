bb51cfde00b3d33fda8a7511d886788850b252ca
gavinzhou <gavin.zhou@gmail.com>
2018-02-06 15:31:05 +0900

    test ci

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,13 @@
-extern crate rand;
+extern crate iron;
 
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
+use iron::prelude::*;
+use iron::status;
 
 fn main() {
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin().read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-        
-        println!("You guessed:{}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
+    fn hello_world(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, "Hello World!")))
     }
+
+    println!("On 3000");
+    Iron::new(hello_world).http("0.0.0.0:3000").unwrap();
 }
